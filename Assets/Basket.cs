747c6613694011780b4ebf29eb3f401a9bf5bf51
Zzznrz <zhongyuhu.zh@gmail.com>
2026-09-09 21:45:14 -0700

    feat: implement scoring system and score display UI

--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Basket : MonoBehaviour
 {
+    public ScoreCounter scoreCounter;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // Find a GameObject named ScoreCounter in the Scene Hierarchy
+        GameObject scoreGO = GameObject.Find( "ScoreCounter" );
+        // Get the ScoreCounter (Script) component of scoreGO
+        scoreCounter = scoreGO.GetComponent<ScoreCounter>();
     }
 
     // Update is called once per frame
@@ -37,6 +42,9 @@ public class Basket : MonoBehaviour
         if ( collidedWith.CompareTag("Apple") )
         {
             Destroy( collidedWith );
+            // Increase the score
+            scoreCounter.score += 100;
+            HighScore.TRY_SET_HIGH_SCORE( scoreCounter.score );
         }
     }
 }
